c969c121ccca91e8067443cf7035ce9f1b623ae8
Emily <emilybribragg@gmail.com>
2021-12-15 17:24:22 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = document.getElementById('new-task-description')
+    console.log(input)
+    console.log(input.value)
+    newToDo(input.value)
+    input.value = ''
+  })
+})
+
+
+function newToDo(todo){
+  let p = document.createElement('p')
+  p.textContent = todo
+  document.querySelector('#tasks').appendChild(p)
+}
